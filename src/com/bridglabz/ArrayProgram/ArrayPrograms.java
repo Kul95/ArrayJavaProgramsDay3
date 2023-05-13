af74480e6ecf66e580f4520f1f1f7e7d7090766f
Kuldeep Kumar <kuldeepkumar17feb@gmail.com>
2023-05-13 16:20:35 +0530

    Added:LasgestElement program

--- src/com/bridglabz/ArrayProgram/ArrayPrograms.java
+++ src/com/bridglabz/ArrayProgram/ArrayPrograms.java
@@ -3,6 +3,7 @@ package com.bridglabz.ArrayProgram;
 public class ArrayPrograms {
     public static void main(String[] args) {
         printArray1();
+        largestArrayElement3();
     }
     public static void printArray1(){
         int arr1[]=new int[10];
@@ -12,7 +13,16 @@ public class ArrayPrograms {
         }
         System.out.println();
     }
-
+    public static void largestArrayElement3(){
+        int arr3[]={1,2,4,3,7,9,5,6};
+        int max= arr3[0];
+        for(int i=0;i<=arr3.length;i++){
+            if(arr3[i]>max){
+                max=arr3[i];
+            }
+            System.out.println("Largest Element is :  "+max);
         }
+    }
+}
 
 
